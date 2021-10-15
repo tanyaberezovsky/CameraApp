730a96516eb8bcd078829dc075f5bc20e4958409
Tanya Berezovsky <tanyab@crow.co.il>
2021-10-15 20:03:02 +0300

    Initial Commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by Tanya Berezovsky on 15/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
